0e0138fabef0a190d3f12621e6256a7ff53ad355
PrajwalThorat <87720694+PrajwalThorat@users.noreply.github.com>
2021-08-15 02:23:23 +0530

    Add files via upload

---
+++ src/main/java/org/email/notification/controller/TextEmailController.java
@@ -0,0 +1,52 @@
+package org.email.notification.controller;
+
+import org.email.notification.AppStart;
+import org.email.notification.model.EmailTemplate;
+import org.email.notification.service.EmailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/v1/notification")
+@Slf4j
+public class TextEmailController {
+
+	private static final Logger log = LoggerFactory.getLogger(EmailService.class);
+
+	@Autowired
+	private EmailService emailService;
+
+	@PostMapping(value="/textemail",consumes = "application/json", produces = "application/json")
+	public String sendEmail(@RequestBody EmailTemplate emailTemplate) {
+		try {
+			log.info("Sending Simple Text Email....");
+			emailService.sendTextEmail(emailTemplate);
+			return "Email Sent!";
+		} catch (Exception ex) {
+			return "Error in sending email: " + ex;
+		}
+	}
+	
+	
+	@PostMapping(value="/attachemail",consumes = "multipart/form-data")
+	public String sendEmailWithAttachment(@RequestPart(value = "file") MultipartFile file) {
+		try {
+			log.info("Sending Attachment Email....");
+			emailService.sendEmailWithAttachment(file);
+			return "Email Sent!";
+		} catch (Exception ex) {
+			return "Error in sending email: " + ex;
+		}
+	}
+	
+
+}
